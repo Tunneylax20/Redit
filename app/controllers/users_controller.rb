f2e28d5f57c4d34912df0d99b88a65758e51ac3c
John Hinrichs <jmhinric@gmail.com>
2014-02-28 12:15:37 -0500

    User can create an account

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(
+      username: params["user"][:username],
+      email: params["user"][:email],
+      password: params["user"][:password],
+      password_confirmation: params["user"][:password_confirmation]
+    )
+    redirect_to user_path(@user)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+end
--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,17 +1,19 @@
 require 'spec_helper'
 
 describe "a user can sign up for an account" do
-  
+  let(:user) { FactoryGirl.build(:user) }
+
   it "can create an account" do
     visit "/"
     click_link "Sign Up"
-    fill_in :username, with: "steve"
-    fill_in :email, with: "steve@gmail.com"
-    fill_in :password, with: "password"
-    fill_in :password_confirmation, with: "password"
+    fill_in "Username", with: user.username
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    fill_in "Password confirmation", with: user.password_confirmation
+    # save_and_open_page
     click_button "Create Account"
 
-    expect(page).to have_content("steve")
+    expect(page).to have_content user.username
   end
 
 end
