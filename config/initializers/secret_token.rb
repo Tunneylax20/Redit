1fa270f92e5f050c8fb872186712a0076c19fa3b
tunneylax20 <bryantunney@gmail.com>
2014-02-28 10:53:47 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Redit::Application.config.secret_key_base = 'cf8c593dbb953e9e15f24c5678467101f3b7472a65bbdd34d1c15706393cca294bc82cbec6f10892fcd563e86de18d15942f7d5d30ad1e21cc1c1157858fd0c4'
